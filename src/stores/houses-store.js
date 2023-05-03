638f66b26750463c92dea93f1c47df8cfed7f2dd
cvrodriguez <cinvir15@gmail.com>
2023-05-03 11:46:57 +0200

    Logic: Add search by city

--- src/stores/houses-store.js
+++ src/stores/houses-store.js
@@ -12,16 +12,16 @@ export const useHousesStore = defineStore('houses-store', () => {
     const newHouseState = ref({})
 
     // Getters
-    const housesByzip = computed(() => housesState.value.filter((h) => h.location.zip.includes(searchCriteria.value)))
+    const housesByzipAndCity = computed(() => housesState.value.filter((h) => h.location.city.toLowerCase().includes(searchCriteria.value.toLowerCase()) || h.location.zip.includes(searchCriteria.value)))
     const house = computed(() => housesState.value.find((h) => h.id === houseIdState.value))
-    const quantityHousesFound = computed(() => housesByzip.value.length)
+    const quantityHousesFound = computed(() => housesByzipAndCity.value.length)
     const sortedHouses = computed(() => {
         if (sortBy.value === 'price') {
-            return housesByzip.value.sort((a, b) => a.price < b.price ? 1 : -1)
+            return housesByzipAndCity.value.sort((a, b) => a.price < b.price ? 1 : -1)
         } else if (sortBy.value === 'size') {
-            return housesByzip.value.sort((a, b) => a.size < b.size ? 1 : -1)
+            return housesByzipAndCity.value.sort((a, b) => a.size < b.size ? 1 : -1)
         } else {
-            return housesByzip.value
+            return housesByzipAndCity.value
         }
     })
 
@@ -49,5 +49,5 @@ export const useHousesStore = defineStore('houses-store', () => {
         await fetchHouses()
     }
 
-    return { housesState, fetchHouses, housesByzip, searchCriteria, quantityHousesFound, sortedHouses, sortBy, house, houseIdState, newHouseState, addNewListing, uploadImage, deleteHouse, editListing }
+    return { housesState, fetchHouses, housesByzipAndCity, searchCriteria, quantityHousesFound, sortedHouses, sortBy, house, houseIdState, newHouseState, addNewListing, uploadImage, deleteHouse, editListing }
 })
